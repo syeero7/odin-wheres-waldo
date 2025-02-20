aefb5f17f2412001863603241127c7d84ee193ec
syeero7 <EmalHasitha@proton.me>
2025-02-20 18:37:40 +0530

    feat: add database schema and Prisma configuration

---
+++ packages/api/prisma/migrations/20250220130407_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Puzzle" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Puzzle_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Character" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "positionX" INTEGER NOT NULL,
+    "positionY" INTEGER NOT NULL,
+    "puzzleId" INTEGER,
+
+    CONSTRAINT "Character_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "HighScores" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+
+    CONSTRAINT "HighScores_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Character" ADD CONSTRAINT "Character_puzzleId_fkey" FOREIGN KEY ("puzzleId") REFERENCES "Puzzle"("id") ON DELETE SET NULL ON UPDATE CASCADE;
