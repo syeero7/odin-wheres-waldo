4b19bb67d9a5ea24717d1ef16f3c978417cac03e
syeero7 <EmalHasitha@proton.me>
2025-03-06 18:20:41 +0530

    feat: update arrow-back SVG icon and Navbar component styles

--- packages/app/src/components/Navbar/Navbar.styled.jsx
+++ packages/app/src/components/Navbar/Navbar.styled.jsx
@@ -2,45 +2,33 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  box-shadow: 0 8px 6px -6px black;
+  box-shadow: 0 8px 6px -6px var(--black-olive);
   margin-bottom: 1em;
-
-  &:has(a:hover),
-  &:has(a:active) {
-    background-image: linear-gradient(to top, hsla(0, 0%, 20%, 0.5), #ddd);
-  }
 `;
 
 export const List = styled.ul`
   display: flex;
   justify-content: space-between;
-  padding: 0.8em 0.5em;
+  padding: 0.5em;
+  margin-right: 0.5em;
 `;
 
 export const ListItem = styled.li`
   display: flex;
   align-items: center;
   transition: transform 0.2s;
-
-  &:has(a:hover),
-  &:has(a:active) {
-    transform: scale(0.95);
-  }
 `;
 
 export const StyledNavLink = styled(NavLink)`
-  font-size: 1.5em;
+  font-size: 1.2em;
   font-weight: 500;
   display: flex;
   align-content: center;
   white-space: nowrap;
-  color: black;
+  color: var(--black);
 
-  &:not(:has(img)) {
-    padding: 0.6em 1em;
-  }
-
-  &:has(img) {
-    padding: 0.25em 0 0.25em 0.5em;
+  & > img {
+    max-width: 1.5em;
+    max-height: 1.5em;
   }
 `;
