4e65ab3b57c10a90857ce4c3ec7b31c667bfa8ef
syeero7 <EmalHasitha@proton.me>
2025-03-06 18:20:04 +0530

    feat: update PuzzleList component styles

--- packages/app/src/components/PuzzleList/PuzzleList.styled.jsx
+++ packages/app/src/components/PuzzleList/PuzzleList.styled.jsx
@@ -5,40 +5,42 @@ export const Container = styled.div`
   margin-bottom: 1em;
 
   @media screen and (min-width: 59em) {
-    margin: 0 1em;
+    margin: 0.5em 1em;
   }
 `;
 
 export const List = styled.ul`
+  --min-col-size: 450px;
+
   display: grid;
   justify-items: center;
   gap: 1em;
   padding: 0.5em;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-
-  @media screen and (min-width: 59em) {
-    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
-  }
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(min(var(--min-col-size), 100%), 1fr)
+  );
 `;
 
 export const ListItem = styled.li`
   & {
     margin: 0.2em;
-    padding: 0.25em;
-    border: hsla(0, 0%, 20%, 0.3) solid 1px;
-    box-shadow: hsla(0, 0%, 20%, 0.4) 5px 5px 6px;
+    padding: 0.5em 0.25em;
+    border: 1px solid hsla(from var(--black-olive) h s l / 0.3);
+    box-shadow: hsla(from var(--black-olive) h s l / 0.4) 5px 5px 6px;
   }
 
   &:has(> a:hover),
   &:has(> a:active),
   &:has(> a:focus-visible) {
-    background-color: #333;
+    border-radius: var(--radius);
+    background-color: var(--black-olive);
   }
 `;
 
 export const StyledLink = styled(Link)`
   display: inline-block;
-  border-radius: 0.5em;
+  border-radius: var(--radius);
   transition: transform 0.2s;
 
   &:hover,
@@ -46,7 +48,7 @@ export const StyledLink = styled(Link)`
   &:focus-visible {
     outline: none;
     transform: scale(0.97);
-    background-color: #fff;
+    background-color: var(--snow);
   }
 `;
 
@@ -54,7 +56,7 @@ export const Image = styled.img`
   min-width: 280px;
   max-width: 100%;
   height: auto;
-  border-radius: 0.5em 0.5em 0 0;
+  border-radius: var(--radius) var(--radius) 0 0;
 `;
 
 export const Characters = styled.div`
@@ -62,6 +64,6 @@ export const Characters = styled.div`
   max-width: fit-content;
   padding: 0.5em;
   gap: 0.5em;
-  background-color: #fff;
-  border-radius: 0.5em;
+  background-color: var(--snow);
+  border-radius: var(--radius);
 `;
