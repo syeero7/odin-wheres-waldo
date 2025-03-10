2de09c0541d1b5003d0b7a012addc1c39dccf5e7
syeero7 <EmalHasitha@proton.me>
2025-03-11 05:06:51 +0530

    build: update Vite configuration to include babel plugin for styled components

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-styled-components", { fileName: false }]],
+      },
+    }),
+  ],
   test: { environment: "jsdom", setupFiles: "./tests/setup.js" },
 });
