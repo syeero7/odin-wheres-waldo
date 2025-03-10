ea18e23ef9895f0c0fd125197d5918d3f6b80bff
syeero7 <EmalHasitha@proton.me>
2025-03-11 04:38:41 +0530

    chore: remove unnecessary async keyword from loader

--- packages/app/src/components/Puzzle/loader.js
+++ packages/app/src/components/Puzzle/loader.js
@@ -1,6 +1,6 @@
 import { validatePuzzleId } from "../../utils/puzzleUtils";
 
-export const loader = async ({ params }) => {
+export const loader = ({ params }) => {
   validatePuzzleId(params.puzzleId);
 
   return { status: 200, ok: true };
