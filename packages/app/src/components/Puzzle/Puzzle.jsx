faa98e196177f38117e67ce34691257b9582f61d
syeero7 <EmalHasitha@proton.me>
2025-03-19 17:06:56 +0530

    refactor: update Puzzle component

--- packages/app/src/components/Puzzle/Puzzle.jsx
+++ packages/app/src/components/Puzzle/Puzzle.jsx
@@ -1,20 +1,15 @@
 import propTypes from "prop-types";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import PuzzleController from "./PuzzleController";
+import PuzzleProvider, {
+  usePuzzleDispatch,
+  usePuzzleState,
+} from "./PuzzleProvider";
 import usePuzzleImages from "../../hooks/usePuzzleImages";
-import useSessionStorage from "../../hooks/useSessionStorage";
 import useBeforeLeave from "../../hooks/useBeforeLeave";
-import useImageElement from "../../hooks/useImageElement";
-import { PuzzleContext } from "./PuzzleContext";
 import { SESSION_STORAGE_KEY, removeItem } from "../../utils/sessionStorage";
-import {
-  getOriginalCoordinates,
-  getClickCoordinates,
-  getImageDimensions,
-  getAdjustedCharacterCoordinates,
-  addFoundStatusToPuzzleCharacters,
-} from "../../utils/puzzleUtils";
+import { getAdjustedCharacterCoordinates } from "../../utils/puzzleUtils";
 import {
   Container,
   Output,
@@ -23,118 +18,91 @@ import {
   MarkersContainer,
   Marker,
   ImageContainer,
-  PuzzleImage,
+  PuzzleImage as Image,
 } from "./Puzzle.styled";
 
 function Puzzle() {
-  const [token, setToken] = useSessionStorage(SESSION_STORAGE_KEY);
-  const [foundCharacters, setFoundCharacters] = useState([]);
-  const [targetPosition, setTargetPosition] = useState({});
-  const [feedback, setFeedback] = useState({ msg: "" });
-  const [menuPosition, setMenuPosition] = useState({});
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { imageRef, imageElement } = useImageElement(
-    token !== null && foundCharacters.length
-  );
+  const { puzzleId } = useParams();
+  const imageRef = useRef(null);
+  const images = usePuzzleImages();
 
   useBeforeLeave(() => {
     removeItem(SESSION_STORAGE_KEY);
   });
 
-  const { puzzleId } = useParams();
-  const images = usePuzzleImages();
-
-  const handleClick = (e) => {
-    const image = imageRef.current;
-    if (!image || e.target !== image) return;
-    const { rect, scaleX, scaleY } = getImageDimensions(image);
-    const clickCoords = getClickCoordinates(e, rect);
-    const originalCoords = getOriginalCoordinates(clickCoords, scaleX, scaleY);
-
-    setIsMenuOpen(true);
-    setMenuPosition(clickCoords);
-    setTargetPosition(originalCoords);
-  };
-
-  const closeMenu = () => setIsMenuOpen(false);
-  const selectedImage = images.find((img) => img.id == puzzleId);
-  const characters = addFoundStatusToPuzzleCharacters(
-    foundCharacters,
-    selectedImage.characters
-  );
-  const adjustedFoundChars = getAdjustedCharacterCoordinates(
-    imageElement,
-    foundCharacters
-  );
-
-  const handleIncorrectGuess = () => setFeedback({ msg: "Incorrect" });
-  const handleCorrectGuess = (token, character) => {
-    const { id, x, y } = character;
-    setFoundCharacters((prev) => [...prev, { id: Number(id), x, y }]);
-    setToken({ token });
-    setFeedback({ msg: "Correct" });
-  };
-  const handleStart = (token) => setToken({ token });
-
-  const value = {
-    token,
-    targetPosition,
-    onStart: handleStart,
-    menu: {
-      characters,
-      isOpen: isMenuOpen,
-      position: menuPosition,
-      close: closeMenu,
-    },
-    onCorrectGuess: handleCorrectGuess,
-    onIncorrectGuess: handleIncorrectGuess,
-  };
+  const selectedImage = images.find((img) => img.id === Number(puzzleId));
 
   return (
-    <Container>
-      <header>
-        <CharacterList>
-          {characters.map(({ id, src, name, hasFound }) => (
-            <Character key={id} style={{ "--opacity": hasFound ? "0.5" : "1" }}>
-              <img src={src} alt={name} width="60" height="60" />
-              <figcaption>{name}</figcaption>
-            </Character>
-          ))}
-        </CharacterList>
-        <Message feedback={feedback} />
-      </header>
+    <PuzzleProvider>
+      <Container>
+        <header>
+          <PuzzleCharacters characters={selectedImage.characters} />
+          <FeedbackMessage />
+        </header>
 
-      <ImageContainer onClick={handleClick}>
-        <PuzzleImage
-          src={selectedImage.src}
-          alt="puzzle"
-          height="768"
-          width="1365"
-          ref={imageRef}
-          fetchPriority="low"
-        />
-
-        <MarkersContainer>
-          {adjustedFoundChars.map(({ id, x, y }) => (
-            <Marker
-              key={id}
-              style={{ "--marker-left": `${x}px`, "--marker-top": `${y}px` }}
-              data-testid="marker"
-            >
-              ✔
-            </Marker>
-          ))}
-        </MarkersContainer>
-
-        <PuzzleContext value={value}>
+        <PuzzleImage src={selectedImage.src} ref={imageRef}>
+          <FoundCharacterMarkers imageRef={imageRef} />
           <PuzzleController />
-        </PuzzleContext>
-      </ImageContainer>
-    </Container>
+        </PuzzleImage>
+      </Container>
+    </PuzzleProvider>
   );
 }
 
-function Message({ feedback }) {
+function PuzzleImage({ src, ref, children }) {
+  const { token } = usePuzzleState();
+  const dispatch = usePuzzleDispatch();
+
+  const handleClick = (e) => {
+    const image = ref.current;
+    if (!image || e.target !== image) return;
+
+    const { clientX, clientY } = e;
+    dispatch({ type: "select_position", payload: { image, clientX, clientY } });
+  };
+
+  return (
+    <ImageContainer onClick={handleClick}>
+      <Image
+        src={src}
+        alt="puzzle"
+        height="768"
+        width="1365"
+        ref={token ? ref : null}
+        fetchPriority="low"
+      />
+
+      {children}
+    </ImageContainer>
+  );
+}
+
+function PuzzleCharacters({ characters }) {
+  const { foundCharacters } = usePuzzleState();
+
+  const foundChars = foundCharacters.reduce((obj, character) => {
+    obj[character.id] = true;
+    return obj;
+  }, {});
+
+  return (
+    <CharacterList>
+      {characters.map(({ id, src, name }) => {
+        const opacity = { "--opacity": foundChars[id] ? "0.5" : "1" };
+
+        return (
+          <Character key={id} style={opacity}>
+            <img src={src} alt={name} width="60" height="60" />
+            <figcaption>{name}</figcaption>
+          </Character>
+        );
+      })}
+    </CharacterList>
+  );
+}
+
+function FeedbackMessage() {
+  const { isGuessCorrect, foundCharacters } = usePuzzleState();
   const [showMessage, setShowMessage] = useState(true);
 
   useEffect(() => {
@@ -144,19 +112,66 @@ function Message({ feedback }) {
     }, 3000);
 
     return () => clearTimeout(timer);
-  }, [feedback]);
+  }, [isGuessCorrect]);
 
-  const variable = feedback.msg === "Correct" ? "--green" : "--red";
+  const message = isGuessCorrect ? "Correct" : "Wrong";
+  const variable = isGuessCorrect ? "--green" : "--red";
 
   return (
     <Output style={{ "--output": `var(${variable})` }}>
-      {showMessage ? feedback.msg : ""}
+      {showMessage && foundCharacters.length ? message : ""}
     </Output>
   );
 }
 
-Message.propTypes = {
-  feedback: propTypes.objectOf(propTypes.string).isRequired,
+function FoundCharacterMarkers({ imageRef }) {
+  const foundCharacters = useAdjustedFoundCharacters(imageRef);
+
+  return (
+    <MarkersContainer>
+      {foundCharacters.map(({ id, x, y }) => {
+        const styles = { "--marker-left": `${x}px`, "--marker-top": `${y}px` };
+
+        return (
+          <Marker key={id} style={styles} data-testid="marker">
+            ✔
+          </Marker>
+        );
+      })}
+    </MarkersContainer>
+  );
+}
+
+const useAdjustedFoundCharacters = (imageRef) => {
+  const [adjustedCharacters, setAdjustedCharacters] = useState([]);
+  const { foundCharacters } = usePuzzleState();
+
+  useEffect(() => {
+    const image = imageRef.current;
+    if (!image) return;
+    const characters = getAdjustedCharacterCoordinates(image, foundCharacters);
+
+    setAdjustedCharacters(characters);
+  }, [imageRef, foundCharacters]);
+
+  return adjustedCharacters;
+};
+
+PuzzleImage.propTypes = {
+  src: propTypes.string.isRequired,
+  ref: propTypes.object.isRequired,
+  children: propTypes.oneOfType([
+    propTypes.arrayOf(propTypes.node).isRequired,
+    propTypes.node.isRequired,
+  ]).isRequired,
+};
+
+PuzzleCharacters.propTypes = {
+  characters: propTypes.arrayOf(propTypes.object.isRequired).isRequired,
+};
+
+FoundCharacterMarkers.propTypes = {
+  imageRef: propTypes.object.isRequired,
 };
 
 export default Puzzle;
